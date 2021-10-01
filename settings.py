816b8cb8cb394989bd07c01eb28b5dc72f41e4b5
Valiice <valentin.claes@student.howest.be>
2021-10-01 22:48:48 +0200

    comment dotenv import needed to be commented for rasppi to work

--- settings.py
+++ settings.py
@@ -3,11 +3,11 @@ DEBUG = True
 if DEBUG:
     print("We are in debug")
     from pathlib import Path
-    from dotenv import load_dotenv
+    # from dotenv import load_dotenv
     from dotenv import dotenv_values
 
     env_path = Path(".") / ".env.debug"
-    load_dotenv(dotenv_path=env_path)
+    # load_dotenv(dotenv_path=env_path)
     env_dict = dotenv_values(dotenv_path=env_path)
     from settings_files._global import *
 else:
